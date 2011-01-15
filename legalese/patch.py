1450e230f5a1efc22ef59ee8c124ae4fc864d36e
Levin Alexander <mail@levinalex.net>
2011-01-15 20:06:34 +0100

    s/create/add

--- legalese/patch.py
+++ legalese/patch.py
@@ -11,19 +11,22 @@ class Patch():
         return self.element.get('document')
 
     def apply(self, store):
+	ops = {"add": self._add}
+
 	from document import Document
         document = store.get(self.document_name)
 	if document is None:
 	    document = Document.empty(self.document_name)
 
-	ops = {"create": self._create}
 	for operation in self.element.iterchildren():
 	    op = ops.get(operation.tag)
 	    op(document, operation)
 
         return {self.document_name: document}
 
-    def _create(self, document, operation):
+
+
+    def _add(self, document, operation):
 	target_path = operation.get("at")
 	target = document.lookup(target_path)
 	for node in operation.iterchildren():
